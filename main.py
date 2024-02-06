8738634b3104da4d9e62a78c35bb1b0914192017
Pierre Marboutin <piballz@gmail.com>
2024-02-06 20:08:51 +0100

    added connexion db

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import tkinter as tk
-import getpass
 import oracledb
 
-pw = getpass.getpass("root")
+conn = oracledb.connect(user='system', password='root', host="localhost", port=1521)
+cursor = conn.cursor()
 
-connection = oracledb.connect(
-    user="system",
-    password=pw,
-    dsn="localhost/xe")
+# Quelle est la plus grosse catégorie de dépense ?
+# cursor.execute('SELECT FK_CATEGORIE, SUM(MONTANT) AS TOTAL_DEPENSE FROM F_TRANSACTION GROUP by FK_CATEGORIE ORDER BY TOTAL_DEPENSE FETCH FIRST 1 ROW ONLY')
+# Quelle est la plus grosse sous catégorie de source de revenue ?
+# cursor.execute('''
+#
+# ''')
+# Quelles est l’évolution du solde client à travers le temps ?
 
-print("Successfully connected to Oracle Database")
+
+# Execute a SELECT statement
+
+
+# Fetch the results
+results = cursor.fetchall()
+
+# Loop through the results
+for row in results:
+    print(row)
+
 
 root = tk.Tk()
 root.title("Transaction bancaire")
+
 
 
 
