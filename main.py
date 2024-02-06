c1d178e7c3dce5e251a13048dc39d9db393f9de9
Pierre Marboutin <piballz@gmail.com>
2024-02-06 21:17:29 +0100

    fixed graphic

--- main.py
+++ main.py
@@ -1,53 +1,60 @@
 import tkinter as tk
+import matplotlib.pyplot as plt
 import oracledb
 
 conn = oracledb.connect(user='system', password='root', host="localhost", port=1521)
 cursor = conn.cursor()
 
 # Quelle est la plus grosse catégorie de dépense ?
-# cursor.execute('SELECT FK_CATEGORIE, SUM(MONTANT) AS TOTAL_DEPENSE FROM F_TRANSACTION GROUP by FK_CATEGORIE ORDER BY TOTAL_DEPENSE FETCH FIRST 1 ROW ONLY')
+cursor.execute('SELECT FK_CATEGORIE, SUM(MONTANT) AS TOTAL_DEPENSE FROM F_TRANSACTION GROUP by FK_CATEGORIE ORDER BY TOTAL_DEPENSE FETCH FIRST 1 ROW ONLY')
 # Quelle est la plus grosse sous catégorie de source de revenue ?
 cursor.execute('''
-SELECT 
-    CASE 
-        WHEN INSTR(FK_CATEGORIE, '-') > 0 THEN
-            SUBSTR(FK_CATEGORIE, INSTR(FK_CATEGORIE, '-', 1, 2) + 2)
-        ELSE
-            SUBSTR(FK_CATEGORIE, INSTR(FK_CATEGORIE, '-') + 2)
-    END AS SOUS_CATEGORIE,
+SELECT
+    SUBSTR(FK_CATEGORIE, INSTR(FK_CATEGORIE, '|') + 2) AS SOUS_CATEGORIE,
     SUM(MONTANT) AS TOTAL_CREDIT
-FROM 
+FROM
     F_TRANSACTION
-WHERE 
+WHERE
     FK_CODE = 'C' -- Sélectionner seulement les opérations de crédit
-GROUP BY 
+GROUP BY
     FK_CATEGORIE
-ORDER BY 
+ORDER BY
     TOTAL_CREDIT DESC
 FETCH FIRST 1 ROWS ONLY
 ''')
 # Quelles est l’évolution du solde client à travers le temps ? Faire un graphique
+cursor.execute('''
+    SELECT FK_DATE, MONTANT AS SOLDE
+    FROM F_TRANSACTION
+    ORDER BY FK_DATE
+''')
 
-
-# Execute a SELECT statement
-
-
-# Fetch the results
 results = cursor.fetchall()
 
-# Loop through the results
-for row in results:
-    print(row)
 
+dates = []
+soldes = []
+
+
+for row in results:
+    dates.append(row[0])
+    soldes.append(row[1])
+
+
+plt.plot(dates, soldes)
+plt.xlabel('Date')
+plt.ylabel('Credit')
+plt.title('Évolution du solde')
+plt.xticks(rotation=45)
+# plt.yscale('symlog')
+plt.tight_layout()
+plt.show()
+
+
+cursor.close()
+conn.close()
 
 root = tk.Tk()
 root.title("Transaction bancaire")
 
-
-
-
-
 root.mainloop()
-
-
-
